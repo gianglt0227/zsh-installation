adfe550041540428c585ff3e04dcc80c5ede8c48
gianglt <gianglt0227@gmail.com>
2018-12-17 15:27:46 +0700

    init

---
+++ install-zsh-root.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+yum -y install zsh git
+wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
+
+# oh-my-zsh plugins
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-completions
+git clone https://github.com/supercrabtree/k ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/k
+cp default-zshrc.zsh-template ~/.zshrc
+chsh -s /bin/zsh $(id -un)
+zsh
+source ~/.zshrc
