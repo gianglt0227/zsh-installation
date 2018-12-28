bed4fc200773158e8bc83cbe40a49eb9ec7ad19b
gianglt <gianglt0227@gmail.com>
2018-12-28 09:51:12 +0700

    simplied user installation

--- install-zsh-user.sh
+++ install-zsh-user.sh
@@ -1,13 +1,3 @@
 #!/bin/sh
-sudo sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
-0
-EOF
-
-# oh-my-zsh plugins
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-completions
-git clone https://github.com/supercrabtree/k ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/k
-cp default-zshrc.zsh-template ~/.zshrc
-zsh
-source ~/.zshrc
+cp -r /root/.oh-my-zsh /home/$1
+cp -r /root/.zshrc     /home/$1
