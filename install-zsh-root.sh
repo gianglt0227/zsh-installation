562efa2a7df731ba9bd3bec8966e04483f6dd6da
gianglt <gianglt0227@gmail.com>
2019-09-08 05:57:56 +0700

    autojump

--- install-zsh-root.sh
+++ install-zsh-root.sh
@@ -9,6 +9,9 @@ git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-~/.oh-my-zs
 git clone https://github.com/supercrabtree/k ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/k
 git clone https://github.com/MichaelAquilina/zsh-you-should-use.git $ZSH_CUSTOM/plugins/you-should-use
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+git clone git://github.com/wting/autojump.git
+cd autojump
+./install.py
 
 cp default-zshrc.zsh-template ~/.zshrc
 chsh -s /bin/zsh $(id -un)
